5cd4106a83b1dfd5d0a644e0ccf58e0622d5f9be
gdas <grdas@outlook.com>
2022-09-05 16:40:10 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,53 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersService } from './servers/servers.service';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth-service.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
+// const appRoutes: Routes = [
+//   { path: '', component: HomeComponent},
+//   { path: 'users', component: UsersComponent, children : [
+//     { path: ':id/:name', component: UserComponent },
+//   ] },  
+//   { path: 'servers', component: ServersComponent, children: [
+//     { path: ':id', component: ServerComponent },
+//     { path: ':id/edit', component: EditServerComponent}]
+//   },
+//   { path: 'not-found', component: PageNotFoundComponent},
+//   { path: '**', redirectTo: '/not-found'}
+// ];
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    HomeComponent,
+    UsersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ServersService } from './servers.service';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  public servers: {id: number, name: string, status: string}[] = [];
+
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+  onReloadPage()
+  {
+    this.router.navigate(['servers'], {relativeTo : this.route});
+    //this.router.navigate(['servers'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+}
